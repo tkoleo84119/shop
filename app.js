325733ff5744e89403220f4dc839525e83d33956
Kerwin <tkoleo84119@gmail.com>
2022-02-22 02:35:40 +0800

    feat: add mongoose to conect mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+'use strict'
+
 const express = require('express')
-require('dotenv').config()
+const mongoose = require('mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
+const port = process.env.PORT || 3000
+const DB_URL = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
 
-const port = process.env.PORT
+// connect to database
+mongoose.connect(DB_URL).then(() => console.log('Connected to database successfully'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(port, () => console.log(`The app is listening on port ${port}`))
+const server = app.listen(port, () => console.log(`The app is listening on port ${port}`))
