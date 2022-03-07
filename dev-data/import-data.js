8d5b38afc71f25cbb91365ace764d7900c363b0c
Kerwin <tkoleo84119@gmail.com>
2022-03-08 01:09:04 +0800

    feat: create import-data.js to import dev-data to DB

---
+++ dev-data/import-data.js
@@ -0,0 +1,44 @@
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const mongoose = require('mongoose')
+const Product = require('../models/productModel')
+require('dotenv').config()
+
+const DB_URL = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+
+// connect to database
+mongoose.connect(DB_URL).then(() => console.log('Connected to database successfully'))
+
+// Read data from file
+const product = JSON.parse(fs.readFileSync(path.join(__dirname, 'products.json')))
+
+// Import data to DB
+const importData = async () => {
+  try {
+    await Product.create(product)
+    console.log('create data success!')
+  } catch (err) {
+    console.log(err)
+  }
+  process.exit()
+}
+
+// Delete data from DB
+const deleteData = async () => {
+  try {
+    await Product.deleteMany()
+    console.log('delete data success!')
+  } catch (err) {
+    console.log(err)
+  }
+  process.exit()
+}
+
+// Use command-line arguments to determine which action will be executed
+if (process.argv[2] === '--import') {
+  importData()
+}
+if (process.argv[2] === '--delete') {
+  deleteData()
+}
